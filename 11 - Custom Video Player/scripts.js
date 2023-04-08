d31def64f317a15d803f6c809bfe03419fc1782f
seunghyune <114329713+SeungHyune@users.noreply.github.com>
2023-04-08 20:38:10 +0900

    11 - Custom Video Player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,43 +1,65 @@
-const player = document.querySelectorAll('.player__button');
-const video = document.querySelector('video');
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
 
-let play = false;
-player.forEach(btn => btn.addEventListener('click', function(e){
-    let currentTime = video.currentTime;
-    let fullTime = video.duration;
+function togglePlay(e){
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
 
-    if(!play && e.target.title.includes("Play")){
-        video.play();
-        play = true;
-    } else if(e.target.title.includes("Play")) {
-        video.pause();
-        play = false;
-    }
+function updateBtnton(){
+    const icon = video.paused ? '►' : '❚ ❚';
+    console.log(icon)
+    toggle.textContent = icon;
+}
 
-    if(e.target.dataset.skip === "-10"){
-        if(currentTime - 10 > 0){
-            currentTime = currentTime - 10;
-        } else {
-            currentTime = currentTime + 25;
-        }
-    } else if(e.target.dataset.skip === "25"){
-        console.log(25)
-    }
-}))
+function skip(){
+    // if(this.dataset['skip'] === "-10"){
+    //     video.currentTime -= 10;
+    // } else if(this.dataset['skip'] === "25"){
+    //     video.currentTime += 25;
+    // }
+    video.currentTime += parseFloat(this.dataset.skip);
+    console.log(video.currentTime)
+}
 
-const videoVolume = document.querySelector('input[name=volume]');
-videoVolume.addEventListener('input', function(e){
-    video.volume = e.target.value * 1;
-})
+function handleRangeUpdate(){
+    // console.log(this.value)
+    // console.log(video.volume)
 
+    // this.name === 'volume' ? video.volume = Number(this.value) : null;
+    // this.name === 'playbackRate' ? video.playbackRate = Number(this.value) : null;
+    video[this.name] = this.value;
+}
 
-const videoRate = document.querySelector('input[name=playbackRate]');
-videoRate.addEventListener('input', function(e){
-    video.playbackRate = e.target.value;
-    console.log(video.playbackRate)
-})
+function handleProgress(e){
+    const percentage = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percentage}%`;
+}
 
-video.addEventListener('click', function(e){
-    console.log(e, e.timeStamp)
-})
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    console.log(scrubTime)
+    video.currentTime = scrubTime;
+}
 
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateBtnton);
+video.addEventListener('pause', updateBtnton);
+video.addEventListener('timeupdate', handleProgress)
+
+toggle.addEventListener('click', togglePlay);
+
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('input', handleRangeUpdate));
+
+progress.addEventListener('mousemove', handleProgress);
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
